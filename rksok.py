e8a94c49d4b34e0654d20252446973fc443632cb
gziltoid <g.ziltoid@gmail.com>
2021-08-31 00:59:21 +0200

    Refactoring

--- rksok.py
+++ rksok.py
@@ -8,7 +8,7 @@ from phonebook import Phonebook
 # TODO: add python prototypes
 
 
-class Method(Enum):
+class RequestMethod(Enum):
     GET = "ОТДОВАЙ"
     PUT = "ЗОПИШИ"
     DELETE = "УДОЛИ"
@@ -20,15 +20,12 @@ class ResponseStatus(Enum):
     BAD_REQUEST = "НИПОНЯЛ"
 
 
-# METHODS = (Method.GET.value, Method.PUT.value, Method.DELETE.value)
 PROTOCOL = "РКСОК/1.0"
 
 PROXY_HOST = "vragi-vezde.to.digital"
 PROXY_PORT = 51624
 SERVER_NAME = "127.0.0.1"
 SERVER_PORT = 8888
-
-# TODO: return None if MOZHNA otherwise Some(str)
 
 
 async def check_with_proxy_server(message) -> Optional[str]:
@@ -39,12 +36,12 @@ async def check_with_proxy_server(message) -> Optional[str]:
     writer.write(request.encode())
     await writer.drain()
 
-    data = await reader.read()
+    response = (await reader.read()).decode()
 
     writer.close()
     await writer.wait_closed()
 
-    return data.decode()
+    return response if response.startswith("НИЛЬЗЯ") else None
 
 
 def format_response(status: ResponseStatus, data: Optional[str] = None) -> str:
@@ -61,50 +58,40 @@ lock = asyncio.Lock()
 async def handle_message(message):
     if message.endswith("\r\n\r\n"):
         message_lines = message.rstrip("\r\n\r\n").split("\r\n")
-        # TODO: try rf""
-        # TODO: use 1 regex instead of 2
-        # match = re.match(
-        #             r"^(ОТДОВАЙ|ЗОПИШИ|УДОЛИ) (.+) РКСОК/1.0", message_lines[0]
-        #         )
-        # if match:
-        #     name = match.group(2)
-        if re.match(r"^(ОТДОВАЙ|ЗОПИШИ|УДОЛИ) ", message_lines[0]) and message_lines[0].endswith(PROTOCOL):
-            response_from_proxy_server = await check_with_proxy_server(message)
-            if response_from_proxy_server.startswith("НИЛЬЗЯ"):
-                return response_from_proxy_server
-            # TODO: check this inside send_request_to_proxy_server
-            elif response_from_proxy_server.startswith("МОЖНА"):
-                entry_name = re.match(
-                    r"^(ОТДОВАЙ|ЗОПИШИ|УДОЛИ) (.+) РКСОК/1.0", message_lines[0]
-                ).group(2)
+        methods = "|".join([x.value for x in RequestMethod])
+        match = re.match(rf"^({methods}) (.+) РКСОК/1.0", message_lines[0])
 
-                if len(entry_name) <= 30:
-                    # TODO: extrach method name, not .startswith
-                    # TODO: use constants
-                    if message.startswith("ОТДОВАЙ"):
-                        # log
-                        async with lock:
-                            phones = phonebook.get_phones_by_name(entry_name)
-                            return (
-                                format_response(ResponseStatus.OK, phones)
-                                if phones
-                                else format_response(ResponseStatus.NOT_FOUND)
-                            )
-                    elif message.startswith("УДОЛИ"):
-                        async with lock:
-                            deleted = phonebook.delete_entry_by_name(
-                                entry_name)
-                            return (
-                                format_response(ResponseStatus.OK)
-                                if deleted
-                                else format_response(ResponseStatus.NOT_FOUND)
-                            )
-                    elif message.startswith("ЗОПИШИ"):
-                        async with lock:
-                            phonebook.add_or_update_entry(
-                                entry_name, "\r\n".join(message_lines[1:])
-                            )
-                            return format_response(ResponseStatus.OK)
+        if match:
+            proxy_response = await check_with_proxy_server(message)
+            if proxy_response:
+                return proxy_response
+
+            request_method, entry_name = match.group(1), match.group(2)
+
+            if len(entry_name) <= 30:
+                if request_method == RequestMethod.GET.value:
+                    # log
+                    async with lock:
+                        phones = phonebook.get_phones_by_name(entry_name)
+                        return (
+                            format_response(ResponseStatus.OK, phones)
+                            if phones
+                            else format_response(ResponseStatus.NOT_FOUND)
+                        )
+                elif request_method == RequestMethod.DELETE.value:
+                    async with lock:
+                        deleted = phonebook.delete_entry_by_name(entry_name)
+                        return (
+                            format_response(ResponseStatus.OK)
+                            if deleted
+                            else format_response(ResponseStatus.NOT_FOUND)
+                        )
+                elif request_method == RequestMethod.PUT.value:
+                    async with lock:
+                        phonebook.add_or_update_entry(
+                            entry_name, "\r\n".join(message_lines[1:])
+                        )
+                        return format_response(ResponseStatus.OK)
 
     return format_response(ResponseStatus.BAD_REQUEST)
 
@@ -118,9 +105,8 @@ async def rksok_handler(reader, writer):
             if not chunk or chunk.endswith(b"\r\n\r\n"):
                 break
     except Exception as e:
-        # TODO: print ERROR
         # log
-        print(type(e), e.args)
+        print("ERROR:", type(e))
     else:
         message = data.decode()
         # log
@@ -128,7 +114,6 @@ async def rksok_handler(reader, writer):
 
         response = await handle_message(message)
 
-        # TODO: ideally don't respond if there's an exception
         # log
         print(f"Sent: {response!r}")
         writer.write(response.encode())
